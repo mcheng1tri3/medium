0b146ec54e084f3db00bc8341e98d0131e9db67c
mcheng1tri3 <mcheng1tri3@gmail.com>
2024-11-07 20:36:33 -0500

    push

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("startButton").addEventListener("click", function() {
+    const image = document.getElementById("image1");
+    image.style.visibility = "visible"; // Show the image
+});
+
+document.getElementById("middleButton").addEventListener("click", function() {
+    const image = document.getElementById("image2");
+    image.style.visibility = "visible"; // Show the image
+});
+
+document.getElementById("lastButton").addEventListener("click", function() {
+    const image = document.getElementById("image3");
+    image.style.visibility = "visible"; // Show the image
+});
